9e4080f2de23ea0fe91b9088a18534feec6ac3fb
Ali-Alaalem <meemo2002a@gmail.com>
2026-03-31 18:18:11 +0300

    adding Role model

---
+++ src/main/java/com/Project3/E_commerce/models/Role.java
@@ -0,0 +1,15 @@
+package com.Project3.E_commerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    private Long id;
+    private String name;
+}
