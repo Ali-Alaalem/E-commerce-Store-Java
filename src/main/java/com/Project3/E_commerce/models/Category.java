e0e183481198a51e3c84665364f2116eaab291a7
Ali-Alaalem <meemo2002a@gmail.com>
2026-03-31 18:23:42 +0300

    adding Category model

---
+++ src/main/java/com/Project3/E_commerce/models/Category.java
@@ -0,0 +1,16 @@
+package com.Project3.E_commerce.models;
+
+;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private Long id;
+    private String name;
+}
