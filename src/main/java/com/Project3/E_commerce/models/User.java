698eca22f707c51898bcee4d85247be321af9935
Ali-Alaalem <meemo2002a@gmail.com>
2026-03-31 18:16:19 +0300

    adding User model

---
+++ src/main/java/com/Project3/E_commerce/models/User.java
@@ -0,0 +1,22 @@
+package com.Project3.E_commerce.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+}
